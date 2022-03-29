87cacc64d2770bae242c0227c845e65a77567e76
maloe19 <maloe19@student.sdu.dk>
2022-03-29 10:35:28 +0200

    moreWork

--- code.py
+++ code.py
@@ -1,1 +1,32 @@
-import matplotlib #https://matplotlib.org/stable/api/index
+#import matplotlib #https://matplotlib.org/stable/api/index
+
+#---------------------------------------------------------
+
+#https://plotly.com/python/getting-started/
+#https://dash.plotly.com/layout
+
+import plotly.express as px
+from dash import Dash, html, Input, dcc
+import pandas as pd
+
+#app  = Dash(__name__)
+
+#app.layout = html.Div([
+    #htl elements
+#])
+
+#@app.callback(
+    #output and input
+#)
+
+#def display():
+    ##simplest form:
+    #fig = px.bar(x=["a","b","c"], y=[3, 2, 1])
+    #fig.show()
+    #return figure
+
+fig = px.bar(x=["a","b","c"], y=[3, 2, 1])
+fig.write_html('first_figure.html', auto_open=True)
+
+#if __name__ == '__main__':
+    #app.run_server(debug=True)
